45b23d3cccf4ed3faabb3f38304a0736ba527e77
Tracy Wallace <tracy.wallace@learnondemandsystems.com>
2021-09-08 12:01:34 -0400

    Added .NET install

--- setup.sh
+++ setup.sh
@@ -1,2 +1,13 @@
 #Install PIP
-sudo apt install -y python3-pip
+#sudo apt install -y python3-pip
+
+#Install .net pre-reqs
+wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+#install SDK
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-5.0
