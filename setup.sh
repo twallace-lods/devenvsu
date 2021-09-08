aa2a2e5414590e9fefd4aae223caff86eb2cae2a
Tracy Wallace <tracy.wallace@learnondemandsystems.com>
2021-09-08 12:13:02 -0400

    Added AWS CLI

--- setup.sh
+++ setup.sh
@@ -2,12 +2,17 @@
 #sudo apt install -y python3-pip
 
 #Install .net pre-reqs
-wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
+#wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+#sudo dpkg -i packages-microsoft-prod.deb
+#rm packages-microsoft-prod.deb
 
 #install SDK
-sudo apt-get update; \
-  sudo apt-get install -y apt-transport-https && \
-  sudo apt-get update && \
-  sudo apt-get install -y dotnet-sdk-5.0
+#sudo apt-get update; \
+#  sudo apt-get install -y apt-transport-https && \
+#  sudo apt-get update && \
+#  sudo apt-get install -y dotnet-sdk-5.0
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
