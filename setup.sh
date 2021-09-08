b111e96e83bd5591cc29b154b831453430a5bcc2
Tracy Wallace <tracy.wallace@learnondemandsystems.com>
2021-09-08 12:30:24 -0400

    Install G++

--- setup.sh
+++ setup.sh
@@ -21,7 +21,10 @@
 #curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 #Install Go
-curl -O https://dl.google.com/go/go1.12.7.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.12.7.linux-amd64.tar.gz
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
-echo "export GOPATH=~/.go" >> ~/.profile
+#curl -O https://dl.google.com/go/go1.12.7.linux-amd64.tar.gz
+#sudo tar -C /usr/local -xzf go1.12.7.linux-amd64.tar.gz
+#echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
+#echo "export GOPATH=~/.go" >> ~/.profile
+
+#Install C++ (G++)
+sudo apt install build-essential
